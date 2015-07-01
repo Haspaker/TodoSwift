239ecb5bae4c021f8520101e782c751d95f72be6
Haspaker <haspaker@haspaker.local>
2015-07-01 13:31:10 +0200

    Initial Commit

---
+++ Todo Swift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Todo Swift
+//
+//  Created by Haspaker on 01/07/15.
+//  Copyright (c) 2015 Haspaker. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Todo Swift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Todo Swift
+//
+//  Created by Haspaker on 01/07/15.
+//  Copyright (c) 2015 Haspaker. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
